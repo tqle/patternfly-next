e12b5a8bd822a0f742e82062843c60128cb2c61b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-06-25 12:23:41 +0000

    Deploy patternfly/patternfly-next to github.com/patternfly/patternfly-next.git:gh-pages

--- path---components-box-examples-9e7ff87cf1525e491bd7.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0x9e9f0043a963],{542:function(o,t){o.exports={pathContext:{type:"component",category:"components",slug:"Box",name:"Box",title:"Box"}}}});
-//# sourceMappingURL=path---components-box-examples-9e7ff87cf1525e491bd7.js.map
