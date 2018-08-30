dd45535e84c2a659fefe707c476a131ea8211571
matthew carleton <mcarleton@redhat.com>
2018-08-30 15:43:21 -0300

    Revert "Feat(forms):a11y updates (#659)"
    This reverts commit 94a9cb8efce909b956d45fd761ad1c5af3ae6b46.

--- src/patternfly/components/FormControl/examples/index.js
+++ src/patternfly/components/FormControl/examples/index.js
@@ -1,33 +1,33 @@
 import React from 'react';
 import Documentation from '@siteComponents/Documentation';
 import Example from '@siteComponents/Example';
-import FormControlInputExampleRaw from '!raw!./form-control-input-example.hbs';
-import FormControlSelectExampleRaw from '!raw!./form-control-select-example.hbs';
-import FormControlTextareaExampleRaw from '!raw!./form-control-textarea-example.hbs';
-import FormControlInputExample from './form-control-input-example.hbs';
-import FormControlSelectExample from './form-control-select-example.hbs';
-import FormControlTextareaExample from './form-control-textarea-example.hbs';
+import FormControlsInputExampleRaw from '!raw!./form-controls-input-example.hbs';
+import FormControlsSelectExampleRaw from '!raw!./form-controls-select-example.hbs';
+import FormControlsTextareaExampleRaw from '!raw!./form-controls-textarea-example.hbs';
+import FormControlsInputExample from './form-controls-input-example.hbs';
+import FormControlsSelectExample from './form-controls-select-example.hbs';
+import FormControlsTextareaExample from './form-controls-textarea-example.hbs';
 import docs from '../docs/code.md';
 import '../styles.scss';
 
 export const Docs = docs;
 
 export default () => {
-  const formControlInputExample = FormControlInputExample();
-  const formControlSelectExample = FormControlSelectExample();
-  const formControlTextareaExample = FormControlTextareaExample();
+  const formControlsInputExample = FormControlsInputExample();
+  const formControlsSelectExample = FormControlsSelectExample();
+  const formControlsTextareaExample = FormControlsTextareaExample();
   const headingText = 'Form Controls';
 
   return (
     <Documentation docs={Docs} heading={headingText}>
-      <Example heading="Input Example" handlebars={FormControlInputExampleRaw}>
-        {formControlInputExample}
+      <Example heading="Input Example" handlebars={FormControlsInputExampleRaw}>
+        {formControlsInputExample}
       </Example>
-      <Example heading="Select Example" handlebars={FormControlSelectExampleRaw}>
-        {formControlSelectExample}
+      <Example heading="Select Example" handlebars={FormControlsSelectExampleRaw}>
+        {formControlsSelectExample}
       </Example>
-      <Example heading="Textarea Example" handlebars={FormControlTextareaExampleRaw}>
-        {formControlTextareaExample}
+      <Example heading="Textarea Example" handlebars={FormControlsTextareaExampleRaw}>
+        {formControlsTextareaExample}
       </Example>
     </Documentation>
   );
