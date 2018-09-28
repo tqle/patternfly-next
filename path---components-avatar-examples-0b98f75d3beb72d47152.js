e9c33f5bb814eaef4ed9e3b0fa40eb1ae741aeab
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-09-28 19:09:53 +0000

    Deploy patternfly/patternfly-next to github.com/patternfly/patternfly-next.git:gh-pages

--- path---components-avatar-examples-0b98f75d3beb72d47152.js
+++ path---components-avatar-examples-0b98f75d3beb72d47152.js
@@ -1,2 +1,2 @@
-webpackJsonp([4276565060998],{791:function(t,a){t.exports={pathContext:{type:"component",category:"components",slug:"Avatar",name:"Avatar",title:"Avatar"}}}});
+webpackJsonp([4276565060998],{805:function(t,a){t.exports={pathContext:{type:"component",category:"components",slug:"Avatar",name:"Avatar",title:"Avatar"}}}});
 //# sourceMappingURL=path---components-avatar-examples-0b98f75d3beb72d47152.js.map
